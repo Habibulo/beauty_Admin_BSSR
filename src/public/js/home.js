024d0f763ffcdcdfaa275d380db8fee2657629f8
Habibullo <hhalimjonov1997@gmail.com>
2024-09-05 23:12:33 +0900

    fix: modified ejs files

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,3 +1,33 @@
+var $pathEls = $("path");
+$pathEls.each(function (i, pathEl) {
+	var offset = anime.setDashoffset(pathEl);
+	$(pathEl).attr("stroke-dashoffset", offset);
+	anime({
+		targets: pathEl,
+		strokeDashoffset: [offset, 0],
+		duration: anime.random(1000, 3000),
+		delay: anime.random(0, 2000),
+		loop: true,
+		direction: "alternate",
+		easing: "easeInOutSine",
+		autoplay: true,
+	});
+});
+
+function toggleDarkMode() {
+	$("body").toggleClass("dark-mode colorful-mode");
+	var $button = $(".dark-mode-toggle");
+	if ($("body").hasClass("colorful-mode")) {
+		$button.text("Dark");
+	} else {
+		$button.text("White");
+	}
+}
+
+
+/**
+	JAVASCRIPT FOR TEST WITHOUT JQUERY
+	
 var pathEls = document.querySelectorAll("path");
 for (var i = 0; i < pathEls.length; i++) {
 	var pathEl = pathEls[i];
@@ -21,6 +51,7 @@ function toggleDarkMode() {
   if (document.body.classList.contains("colorful-mode")) {
       button.textContent = " Dark ";
   } else {
-      button.textContent = "Rainbow";
+      button.textContent = "White";
   }
 }
+	**/
